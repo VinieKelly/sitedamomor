ff57ffab3a4587fbd71d56a7b322a61b4878fe30
VinieKelly <vdroid678@gmail.com>
2026-06-30 21:08:05 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# criar primeira pagina do site
+
+#route
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
+
+# colocar site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    #servidor do heroku
